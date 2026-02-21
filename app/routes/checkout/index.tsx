e26fe39186ff1053ef0237ad3b5d1ebe17b07671
Davonne Vigil <davonnejv@gmail.com>
2026-02-20 22:42:54 -0600

    add: cart items to check out page

--- app/routes/checkout/index.tsx
+++ app/routes/checkout/index.tsx
@@ -1,10 +1,19 @@
 import { CakeSlice } from "lucide-react";
+import { useCheesecakeItems } from "~/context/OurCheesecake";
+import { useCart } from "~/context/CartContext";
 
 export default function CheckoutPage() {
+  const { cheesecakeItem } = useCheesecakeItems();
+  const { cart, deleteFromCart, clearCart } = useCart();
   return (
     <section className="mt-20 mx-auto">
-      <h1>Your Order</h1>
-      <form className="flex flex-col justify-center mt-40 items-center gap-4 bg-black/70 max-w-xs md:max-w-lg lg:max-w-4xl mx-auto p-8 md:p-8">
+      <h1 className="text-center text-4xl font-nic mt-30">Your Order</h1>
+      <ul>
+        {cart.map((items) => (
+          <li className="text-black">{items.name}</li>
+        ))}
+      </ul>
+      <form className="flex flex-col justify-center mt-40 items-center gap-4 bg-black/70 rounded-3xl max-w-xs md:max-w-lg lg:max-w-4xl mx-auto p-8 md:p-8">
         <h1 className="font-nic text-5xl text-white">
           <div className="flex gap-4 items-center">
             <CakeSlice width={30} height={30} />
