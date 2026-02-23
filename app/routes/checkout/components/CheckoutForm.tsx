7dbab60aa78604f2cce78f9ba90c8f9c69cce7f3
Davonne Vigil <davonnejv@gmail.com>
2026-02-22 20:38:07 -0600

    feature: form functions handleChange and handleSubmit

--- app/routes/checkout/components/CheckoutForm.tsx
+++ app/routes/checkout/components/CheckoutForm.tsx
@@ -1,29 +1,74 @@
 import { CakeSlice } from "lucide-react";
+import { useState } from "react";
+
+type FormData = {
+  fullName: string;
+  address: string;
+  email: string;
+  cardNumber: string;
+  expiry: string;
+  cvv: string;
+  promo: string;
+};
 
 export default function CheckoutForm() {
+  const [formData, setFormData] = useState<FormData>({
+    fullName: "",
+    address: "",
+    email: "",
+    cardNumber: "",
+    expiry: "",
+    cvv: "",
+    promo: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Order Placed:", formData);
+    alert(
+      "Success, order has been placed. Thank you, see you later, its cheesecake time!",
+    );
+  };
+
   return (
     <form
-      className="mt-10 bg-black/70 rounded-3xl 
-      max-w-xs md:max-w-5xl lg:max-w-7xl 
-      mx-auto p-10"
+      onSubmit={handleSubmit}
+      className="mt-12 bg-black/70 rounded-3xl 
+    
+      mx-auto p-8 max-w-7xl"
     >
       <h1
         className="font-nic text-5xl text-white 
-         flex gap-4 items-center justify-center mb-10"
+         flex gap-4 items-center justify-center "
       >
         <CakeSlice width={30} height={30} />
         Checkout
         <CakeSlice width={30} height={30} />
       </h1>
+      <p className="text-white max-w-sm text-center p-2 mx-auto">
+        {" "}
+        And every order comes with a side of apple pie and ice cream on us.
+      </p>
 
-      <div className="flex flex-col md:flex-row gap-10 md:gap-20 items-center justify-between">
+      <div className="flex flex-col md:flex-row gap-10 md:gap-20 items-center justify-between mt-8">
         <div className="flex flex-col gap-8 font-dm w-full md:w-1/2">
           <input
             type="text"
+            required
+            onChange={handleChange}
             placeholder="Full Name"
             className="border-3 border-[#FE7F9C] p-2 bg-white rounded-sm outline-none"
           />
           <input
+            required
+            onChange={handleChange}
             type="address"
             placeholder="Address"
             className="border-3 border-[#FE7F9C] p-2 bg-white rounded-sm outline-none"
@@ -31,17 +76,39 @@ export default function CheckoutForm() {
 
           <input
             type="email"
+            required
+            onChange={handleChange}
             placeholder="E-mail Address"
             className="border-3 border-[#FE7F9C] p-2 bg-white rounded-sm outline-none"
           />
 
           <input
+            required
+            onChange={handleChange}
             type="text"
             placeholder="Credit Card Number"
             className="border-3 border-[#FE7F9C] p-2 bg-white rounded-sm outline-none"
           />
+          <div className="flex gap-4">
+            <input
+              required
+              onChange={handleChange}
+              type="text"
+              placeholder="expir"
+              className="border-3 border-[#FE7F9C] p-2 bg-white rounded-sm outline-none w-20"
+            />
+
+            <input
+              required
+              onChange={handleChange}
+              type="text"
+              placeholder="cvv"
+              className="border-3 border-[#FE7F9C] p-2 bg-white rounded-sm outline-none w-20"
+            />
+          </div>
 
           <input
+            onChange={handleChange}
             type="text"
             placeholder="PROMO CODE"
             className="border-3 border-[#FE7F9C] p-2 bg-white rounded-sm outline-none"
@@ -55,7 +122,7 @@ export default function CheckoutForm() {
         <div className="w-full md:w-1/2 flex justify-center">
           <img
             src="/images/applePie.webp"
-            className="w-60 md:w-96 rounded-2xl"
+            className="w-80 md:w-md rounded-2xl"
             alt="Apple Pie"
           />
         </div>
--- app/routes/checkout/components/Order.tsx
+++ app/routes/checkout/components/Order.tsx
@@ -20,12 +20,13 @@ export default function Order() {
     .reduce((acc, product) => acc + product.price * product.qty, 0)
     .toFixed(2);
   return (
-    <section className="flex flex-col justify-center items-center ">
-      <h1 className="text-center text-4xl md:text-5xl font-nic mt-10 md:mt-20">
+    <section className="flex flex-col justify-center items-center w-full ">
+      <h1 className="text-center text-4xl md:text-5xl font-nic mt-8">
         Your Cart
       </h1>
       <p className="font-thin text-md mx-auto md:text-2xl mt-4 text-center max-w-xs md:max-w-lg">
-        And every order comes with a side of apple pie and ice cream on us.
+        Once order is placed, your order will be ready in 15 minutes for pick
+        up!
       </p>
 
       {cart.length === 0 ? (
@@ -49,7 +50,7 @@ export default function Order() {
         <section>
           <ul>
             {cart.map((cheesecake) => (
-              <div className="flex flex-col bg-white w-full p-4 rounded-2xl mt-8 gap-8 font-dm text-lg md:text-xl">
+              <div className="flex flex-col bg-white w-full p-4 rounded-2xl mt-8 gap-8 font-dm text-md md:text-xl">
                 <div className="flex justify-between gap-20 md:gap-60">
                   <li key={cheesecake.id}>{cheesecake.name}</li>
                   <div className="flex gap-4">
@@ -59,7 +60,7 @@ export default function Order() {
                   </div>
                 </div>
 
-                <div className="flex  gap-4">
+                <div className="flex gap-4">
                   <button
                     onClick={() => scrollToSection("collection")}
                     className="outline-1 p-2 rounded-lg cursor-pointer hover:scale-105"
@@ -77,8 +78,8 @@ export default function Order() {
             ))}
           </ul>
 
-          <div className="flex flex-col md:flex-row justify-center items-center mt-8 md:gap-10 ">
-            <button className="font-bold cursor-pointer text-black text-xl p-2 rounded-lg">
+          <div className="flex flex-col md:flex-row justify-center items-center mt-10 md:gap-10 ">
+            <button className="font-bold cursor-pointer text-black text-lg md:text-2xl p-2 rounded-lg">
               Total:${total}
             </button>
             <button
